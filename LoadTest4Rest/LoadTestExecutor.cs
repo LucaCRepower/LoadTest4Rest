badb17b40f6ea1b91e823fbefc0a848bf33ddcf5
Luca Cavallari <luca.cavallari@repower.com>
2018-09-03 16:20:22 +0200

    Scaffolding per Esecutore

---
+++ LoadTest4Rest/LoadTestExecutor.cs
@@ -0,0 +1,29 @@
+﻿using com.Repower.LoadTest4Rest.entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.Repower.LoadTest4Rest
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LoadTestExecutor
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="visitors"></param>
+        /// <param name="job2Execute"></param>
+        /// <returns></returns>
+        public List<FeedbackInfo> Execute(string url, int visitors, JobInfo job2Execute)
+        {
+            return null;
+        }
+    }
+}
